8c39efa16894e2551573050f48901d72f74245d1
mshivashankar <mithun.shivashankar@teskalabs.com>
2022-12-16 04:43:47 +0100

    throw runtime error

--- asabiris/orchestration/sendslack.py
+++ asabiris/orchestration/sendslack.py
@@ -11,10 +11,16 @@ class SendSlackOrchestrator(object):
 
 
 	def __init__(self, app):
-
+		# formatters
 		self.JinjaService = app.get_service("JinjaService")
+		# output
+		self.SlackOutputService = app.get_service("SlackOutputService")
+
 
 	async def send_to_slack(self, msg):
-		body = msg.get("body")
-		body = await self.JinjaService.format(body['template'], body['params'])
-		await self.App.SlackOutputService.send(body)
+		body = msg.get("body", None)
+		if body is not 	None:
+			body = await self.JinjaService.format(body['template'], body['params'])
+			await self.SlackOutputService.send(body)
+		else:
+			raise RuntimeError("Failed to send message to Slack. Reason: Missing body info.")
