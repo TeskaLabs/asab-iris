5bfacd47036ca5eb734709e3c493cf2f6fa1881a
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-01-12 06:05:13 +0100

    Check / exists

--- asabiris/orchestration/sendmail.py
+++ asabiris/orchestration/sendmail.py
@@ -71,6 +71,7 @@ class SendMailOrchestrator(object):
 
 					# get file-name of the attachment
 					file_name = self.get_file_name(a)
+					assert template[:1] == "/"
 					template_path = self.TempPath + template
 					jinja_output, result = await self.render(template_path, params)
 
