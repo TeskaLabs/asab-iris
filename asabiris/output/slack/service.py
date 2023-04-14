16d58c44abc9c7e653d28083e4a097ebfd21f9f8
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-04-14 10:35:21 +0200

    New update send body as initial comment

--- asabiris/output/slack/service.py
+++ asabiris/output/slack/service.py
@@ -16,11 +16,11 @@ L = logging.getLogger(__name__)
 class SlackOutputService(asab.Service, OutputABC):
     def __init__(self, app, service_name="SlackOutputService"):
         super().__init__(app, service_name)
-
         try:
             self.SlackWebhookUrl = asab.Config.get("slack", "webhook_url")
             self.Client = WebClient(token=self.SlackWebhookUrl)
-            self.Channel = asab.Config.get("slack_channel", None)
+            self.Channel = asab.Config.get("slack", "slack_channel")
+
         except configparser.NoOptionError as e:
             L.error("Please provide webhook_url in slack configuration section.")
             raise e
@@ -47,11 +47,6 @@ class SlackOutputService(asab.Service, OutputABC):
             return
 
         try:
-            self.Client.chat_postMessage(
-                channel=self.Channel,
-                text=body
-            )
-
             if len(atts) != 0:
                 for attachment in atts:
                     file_content = attachment[0].encode('utf-8')
@@ -59,6 +54,7 @@ class SlackOutputService(asab.Service, OutputABC):
                     self.Client.files_upload(
                         channels=self.Channel,
                         file=file_obj,
+                        initial_comment=body,
                         filetype=attachment[1],
                         filename=attachment[2],
                         title=attachment[2]
