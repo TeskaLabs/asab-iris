f04597c3d9c9bb4545c6d3e2914fbdc0434833a7
eliska <eliska.novotna@teskalabs.com>
2023-06-21 17:19:40 +0200

    create_nested_dict_from_dots_in_keys

--- asabiris/formatter/jinja/service.py
+++ asabiris/formatter/jinja/service.py
@@ -25,4 +25,33 @@ class JinjaFormatterService(asab.Service, FormatterABC):
 			raise KeyError("Template '{}' not found".format(template_path))
 
 		template = jinja2.Template(template_io.read().decode('utf-8'))
+
+		# This creates nested dictionary from keys with dot notation
+		template_params = create_nested_dict_from_dots_in_keys(template_params)
 		return template.render(**template_params)
+
+
+def create_nested_dict_from_dots_in_keys(data):
+	"""
+	This function creates a nested dictionary from a dictionary with keys containing dots.
+
+	:param data: The input dictionary that may contain keys with dots in them, indicating nested levels
+	of dictionaries
+	:return: a nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
+	"""
+	nested_dict = {}
+	for key, value in data.items():
+		if isinstance(value, dict):
+			value = create_nested_dict_from_dots_in_keys(value)
+
+		if '.' in key:
+			parts = key.split('.')
+			current_dict = nested_dict
+			for part in parts[:-1]:
+				current_dict.setdefault(part, {})
+				current_dict = current_dict[part]
+
+			current_dict[parts[-1]] = value
+		else:
+			nested_dict[key] = value
+	return nested_dict
