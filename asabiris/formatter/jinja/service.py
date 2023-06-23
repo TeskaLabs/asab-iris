cb0e6d3fdcc837f90e69bfd5f85674a01a17f5bb
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-06-23 08:53:12 +0200

    Use stack instead of recursion

--- asabiris/formatter/jinja/service.py
+++ asabiris/formatter/jinja/service.py
@@ -9,15 +9,14 @@ from ...formater_abc import FormatterABC
 
 L = logging.getLogger(__name__)
 
+
 #
 
 
 class JinjaFormatterService(asab.Service, FormatterABC):
 
-
 	def __init__(self, app, service_name="JinjaService"):
 		super().__init__(app, service_name)
-
 
 	async def format(self, template_path, template_params):
 		template_io = await self.App.LibraryService.read(template_path)
@@ -37,21 +36,22 @@ def create_nested_dict_from_dots_in_keys(data):
 
 	:param data: The input dictionary that may contain keys with dots in them, indicating nested levels
 	of dictionaries
-	:return: a nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
+	:return: A nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
 	"""
-	nested_dict = {}
-	for key, value in data.items():
-		if isinstance(value, dict):
-			value = create_nested_dict_from_dots_in_keys(value)
+	nested_dict = {}  # Initialize an empty nested dictionary
+	stack = [(nested_dict, key, value) for key, value in data.items()]  # Create a stack of (dictionary, key, value) tuples
 
-		if '.' in key:
-			parts = key.split('.')
-			current_dict = nested_dict
+	while stack:
+		current_dict, key, value = stack.pop()  # Pop the top (dictionary, key, value) tuple from the stack
+
+		if '.' in key:  # If the key contains a dot, indicating nested levels
+			parts = key.split('.')  # Split the key into parts using dot as the separator
+
+			# Traverse the nested levels of dictionaries
 			for part in parts[:-1]:
-				current_dict.setdefault(part, {})
-				current_dict = current_dict[part]
-
-			current_dict[parts[-1]] = value
+				current_dict = current_dict.setdefault(part, {})  # Create or get the nested dictionary
+			current_dict[parts[-1]] = value  # Assign the value to the final nested level
 		else:
-			nested_dict[key] = value
-	return nested_dict
+			current_dict[key] = value  # If no dot in the key, assign the value directly to the current dictionary
+
+	return nested_dict  # Return the resulting nested dictionary
