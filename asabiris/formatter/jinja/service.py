895e81af7aaf7f5ff187d2eaf07d8b0d59d4a3b3
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-06-26 09:28:40 +0200

    Stck data-stucture works correctly

--- asabiris/formatter/jinja/service.py
+++ asabiris/formatter/jinja/service.py
@@ -26,32 +26,43 @@ class JinjaFormatterService(asab.Service, FormatterABC):
 		template = jinja2.Template(template_io.read().decode('utf-8'))
 
 		# This creates nested dictionary from keys with dot notation
-		template_params = create_nested_dict_from_dots_in_keys(template_params)
+		template_params = self.create_nested_dict_from_dots_in_keys(template_params)
 		return template.render(**template_params)
 
+	def create_nested_dict_from_dots_in_keys(self, data):
+		"""
+		This function creates a nested dictionary from a dictionary with keys containing dots.
 
-def create_nested_dict_from_dots_in_keys(data):
-	"""
-	This function creates a nested dictionary from a dictionary with keys containing dots.
+		:param data: The input dictionary that may contain keys with dots in them, indicating nested levels
+		of dictionaries
+		:return: a nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
+		"""
+		"""
+		This function creates a nested dictionary from a dictionary with keys containing dots.
 
-	:param data: The input dictionary that may contain keys with dots in them, indicating nested levels
-	of dictionaries
-	:return: a nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
-	"""
-	nested_dict = {}
-	stack = [(nested_dict, data)]
+		:param data: The input dictionary that may contain keys with dots in them, indicating nested levels
+		of dictionaries
+		:return: a nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
+		"""
+		nested_dict = {}
+		stack = [(nested_dict, data)]
 
-	while stack:
-		current_dict, current_data = stack.pop()
-		for key, value in current_data.items():
-			if isinstance(value, dict):
-				stack.append((current_dict.setdefault(key, {}), value))
-			elif '.' in key:
-				parts = key.split('.')
-				for part in parts[:-1]:
-					current_dict = current_dict.setdefault(part, {})
-				current_dict[parts[-1]] = value
-			else:
-				current_dict[key] = value
+		while stack:
+			current_dict, current_data = stack.pop()
 
-	return nested_dict
+			for key, value in current_data.items():
+				if isinstance(value, dict):
+					current_dict[key] = {}
+					stack.append((current_dict[key], value))
+				elif '.' in key:
+					parts = key.split('.')
+					temp_dict = current_dict
+					for part in parts[:-1]:
+						if part not in temp_dict:
+							temp_dict[part] = {}
+						temp_dict = temp_dict[part]
+					temp_dict[parts[-1]] = value
+				else:
+					current_dict[key] = value
+
+		return nested_dict
