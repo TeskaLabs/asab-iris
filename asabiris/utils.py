17304ec77ed224095424ba9ee5b0a05ec5a1287e
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-06-29 09:34:52 +0200

    Add helper method

--- asabiris/utils.py
+++ asabiris/utils.py
@@ -33,43 +33,49 @@ def normalize_body(body):
 
 def create_nested_dict_from_dots_in_keys(data):
 	"""
-	Create a nested dictionary from a dictionary with keys containing dots.
+	This function creates a nested dictionary from a dictionary with keys containing dots.
+	It uses a stack to keep track of dictionaries that need to be processed, avoiding recursion.
 
-	This function takes an input dictionary `data` that may contain keys with dots, indicating nested levels of dictionaries.
-	It splits the keys containing dots and constructs a nested dictionary structure accordingly.
-
-	:param data: The input dictionary that may contain keys with dots in them.
-	:return: A nested dictionary where keys containing dots have been split into sub-dictionaries.
-
-	Example:
-	input: {'alert.event.id': 1, 'alert2': {'event': {'id': 2}}}
-	output: {'alert': {'event': {'id': 1}}, 'alert2': {'event': {'id': 2}}}
+	:param data: The input dictionary that may contain keys with dots in them, indicating nested levels
+	of dictionaries. For example:
+	:return: a nested dictionary where keys containing dots (".") have been split into sub-dictionaries.
 	"""
-	nested_dict = {}  # Initialize the nested dictionary
-	stack = [(nested_dict, data)]  # Use a stack to keep track of dictionaries and their corresponding data
-	keys_to_process = list(data.keys())  # Store the keys to process separately
 
+	# Helper function to insert a value into the nested dictionary at the location specified by the list of keys
+	def insert_nested_dict(keys, value, nested_dict):
+		for key in keys[:-1]:
+			# Ensure that the dictionary at the current level has a key for the next level
+			nested_dict = nested_dict.setdefault(key, {})
+		# Insert the value at the final level
+		nested_dict[keys[-1]] = value
+
+	# Initialize the nested dictionary and the stack with the input data
+	nested_dict = {}
+	stack = [(list(data.items()), nested_dict)]
+
+	# Process dictionaries on the stack
 	while stack:
-		current_dict, current_data = stack.pop()
-
-		for key in keys_to_process:
-			value = current_data[key]
-
-			parts = key.split('.')  # Split the key by dot ('.') delimiter
-			nested = current_dict
-
-			# Traverse the nested dictionary structure and create nested dictionaries as needed
-			for part in parts[:-1]:
-				if part not in nested:
-					nested[part] = {}
-				nested = nested[part]
-
-			nested[parts[-1]] = value  # Assign the value to the last part of the key in the nested dictionary
-
-			if isinstance(value, dict):
-				stack.append(
-					(nested[parts[-1]], value))  # Add nested dictionaries to the stack for further processing
-
-		keys_to_process.clear()  # Clear the processed keys
-
+		current_data, current_dict = stack.pop()
+		for key, value in current_data:
+			if '.' in key:
+				# Split the key into parts and create nested dictionaries for each part
+				keys = key.split('.')
+				if isinstance(value, dict):
+					# If the value is a dictionary, add it to the stack to be processed later
+					new_dict = {}
+					stack.append((list(value.items()), new_dict))
+					insert_nested_dict(keys, new_dict, current_dict)
+				else:
+					# If the value is not a dictionary, insert it directly into the nested dictionary
+					insert_nested_dict(keys, value, current_dict)
+			else:
+				# If the key does not contain a dot, handle it as a normal key-value pair
+				if isinstance(value, dict):
+					# If the value is a dictionary, add it to the stack to be processed later
+					new_dict = {}
+					stack.append((list(value.items()), new_dict))
+					current_dict[key] = new_dict
+				else:
+					# If the value is not a dictionary, insert it directly into the nested dictionary
+					current_dict[key] = value
 	return nested_dict
