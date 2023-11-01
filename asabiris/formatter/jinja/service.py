8a4484bc5bdedf574a9df8444d0c51caf6d0f131
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-11-01 04:09:06 +0100

    working dict sensitive

--- asabiris/formatter/jinja/service.py
+++ asabiris/formatter/jinja/service.py
@@ -13,6 +13,17 @@ from ... import utils
 L = logging.getLogger(__name__)
 
 
+class CaseInsensitiveDict(dict):
+    """
+    A dictionary subclass that provides case-insensitive access to its keys.
+    """
+    def __getitem__(self, key):
+        return super().__getitem__(key.upper())
+
+    def get(self, key, default=None):
+        return super().get(key.upper(), default)
+
+
 class JinjaFormatterService(asab.Service, FormatterABC):
     """
     JinjaFormatterService is responsible for formatting templates using Jinja2.
@@ -20,50 +31,35 @@ class JinjaFormatterService(asab.Service, FormatterABC):
     """
 
     def __init__(self, app, service_name="JinjaService"):
-        """
-        Initialize the JinjaFormatterService.
-
-        :param app: The application instance.
-        :param service_name: The name of the service.
-        """
         super().__init__(app, service_name)
+        self.Variables = CaseInsensitiveDict()
 
         # Load variables from the configuration section named 'variables'
         try:
-            self.Variables = {option.upper(): asab.Config.get('variables', option) for option in asab.Config.options('variables')}
+            config_vars = {option.upper(): asab.Config.get('variables', option) for option in asab.Config.options('variables')}
+            self.Variables.update(config_vars)
         except configparser.NoSectionError:
-            self.Variables = {}
+            self.Variables = CaseInsensitiveDict()  # No variables section in the config, continue with an empty Variables dictionary
 
         # Load variables from JSON file if specified in the configuration
         json_path = asab.Config.get('jinja', 'variables', fallback=None)
         if json_path and os.path.exists(json_path):
             with open(json_path, 'r') as json_file:
-                self.Variables.update({k.upper(): v for k, v in json.load(json_file).items()})
+                json_data = {k.upper(): v for k, v in json.load(json_file).items()}
+                self.Variables.update(json_data)  # This will overwrite any conflicting keys from the config
 
-    def __getitem__(self, key):
-        """
-        Retrieve the value of a variable using the square bracket notation.
-
-        :param key: The name of the variable.
-        :return: The value of the variable or None if not found.
-        """
-        return self.Variables.get(key.upper(), None)
+        self.Variables = CaseInsensitiveDict(self.Variables)
 
     async def format(self, template_path, template_params):
-        """
-        Format a given template using Jinja2.
+        # Convert template_params to be case-insensitive
+        template_params = CaseInsensitiveDict(template_params)
 
-        :param template_path: The path to the Jinja2 template.
-        :param template_params: The parameters to be used in the template.
-        :return: The formatted template.
-        """
         # Combine the provided template_params with the loaded Variables
         jinja_variables = collections.ChainMap(template_params, self.Variables)
-
         # Read the template from the specified template_path
         template_io = await self.App.LibraryService.read(template_path)
         if template_io is None:
-            raise PathError("Template '{}' not found".format(template_path))
+            raise PathError(f"Template '{template_path}' not found")
 
         # Create a Jinja2 template from the read content
         template = jinja2.Template(template_io.read().decode('utf-8'))
