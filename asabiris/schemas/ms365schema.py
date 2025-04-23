3d55ebf1adb1c0f0061158ed57bcdb5635f9876e
Mithun Shivashankar <mithun.shivashankar@teskalabs.com>
2025-04-23 09:14:52 +0200

    Fix the schema

--- asabiris/schemas/ms365schema.py
+++ asabiris/schemas/ms365schema.py
@@ -1,55 +1,55 @@
 ms365_schema = {
-  "type": "object",
-  "required": [
-    "to",
-    "body"
-  ],
-  "properties": {
-    "to": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      }
-    },
-    "cc": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "default": []
-    },
-    "bcc": {
-      "type": "array",
-      "items": {
-        "type": "string"
-      },
-      "default": []
-    },
-    "subject": {
-      "type": "string"
-    },
-    "from": {
-      "type": "string"
-    },
-    "tenant": {
-      "type": "string",
-      "default": ""
-    },
-    "body": {
-      "type": "object",
-      "required": [
-        "template"
-      ],
-      "properties": {
-        "template": {
-          "type": "string",
-          "default": "/Templates/MS365/alert.md"
+    "type": "object",
+    "required": [
+        "to",
+        "body"
+    ],
+    "properties": {
+        "to": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
         },
-        "params": {
-          "type": "object",
-          "default": {}
+        "cc": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "default": []
+        },
+        "bcc": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "default": []
+        },
+        "subject": {
+            "type": "string"
+        },
+        "from": {
+            "type": "string"
+        },
+        "tenant": {
+            "type": "string",
+            "default": ""
+        },
+        "body": {
+            "type": "object",
+            "required": [
+                "template"
+            ],
+            "properties": {
+                "template": {
+                    "type": "string",
+                    "default": "/Templates/MS365/alert.md"
+                },
+                "params": {
+                    "type": "object",
+                    "default": {}
+                }
+            }
         }
-      }
-    },
-  }
+    }
 }
