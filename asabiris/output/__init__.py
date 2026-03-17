fe17eb42e99e21fe7aee4003763fb2a9e014e50b
Mithun Shivashankar <mithun.shivashankar@teskalabs.com>
2026-03-17 04:55:31 +0100

    update output init

--- asabiris/output/__init__.py
+++ asabiris/output/__init__.py
@@ -3,11 +3,13 @@ from .slack.service import SlackOutputService
 from .sms.service import SMSOutputService
 from .msteams.service import MSTeamsOutputService
 from .ms365.service import M365EmailOutputService
+from .mattermost.service import MattermostOutputService
 
 __all__ = [
 	"EmailOutputService",
 	"SlackOutputService",
 	"MSTeamsOutputService",
 	"SMSOutputService",
-	"M365EmailOutputService"
+	"M365EmailOutputService",
+	"MattermostOutputService",
 ]
